b15ac0eadd3e5c2715f44e2c111535f4b79ada41
alessandrokonrad <akonrad6@gmail.com>
2022-03-18 08:41:01 +0100

    fixed default host

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   /* Ogmios connection: https://ogmios.dev/api/interfaces/_cardano_ogmios_client.ConnectionConfig.html */
   connection: {
-    host: '', // e.g. 192.168.178.30; leave blank to use localhost
+    host: null, // e.g. 192.168.178.30; leave blank to use localhost
     port: null, // default port 1337
     tls: false,
   },
